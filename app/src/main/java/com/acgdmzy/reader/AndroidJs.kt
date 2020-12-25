edcd8cf43093e491dc03d3866f72e82729a176a3
wuyu <wuyu8512@gmail.com>
2020-12-25 22:12:57 +0800

    配合web实现打开文件

--- app/src/main/java/com/acgdmzy/reader/AndroidJs.kt
+++ app/src/main/java/com/acgdmzy/reader/AndroidJs.kt
@@ -1,17 +1,20 @@
 package com.acgdmzy.reader
 
-import android.content.ContentValues.TAG
+import android.util.Base64
 import android.util.Log
 import android.webkit.JavascriptInterface
+import android.widget.Toast
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileNotFoundException
 import java.io.IOException
 import java.lang.System.`in`
-import java.util.*
+
 
 // 继承自Object类
 class AndroidJs constructor(private val activity: MainActivity) : Any() {
+
+    private val tag = "AndroidJs"
 
     // 定义JS需要调用的方法
     // 被JS调用的方法必须加入@JavascriptInterface注解
@@ -22,20 +25,33 @@ class AndroidJs constructor(private val activity: MainActivity) : Any() {
 
     @JavascriptInterface
     fun readFile(path: String): String? {
-        return Base64.getEncoder().encodeToString(readFileToByteArray(path))
+        if (path.startsWith("content://")) {
+            val inStream = activity.contentResolver.openInputStream(activity.intent.data!!)
+            if (inStream != null) {
+                val buffer = ByteArray(inStream.available())
+                inStream.read(buffer)
+                return Base64.encodeToString(buffer, Base64.NO_WRAP)
+            }
+        }
+        return Base64.encodeToString(readFileToByteArray(path), Base64.NO_WRAP)
+    }
+
+    @JavascriptInterface
+    fun toast(msg: String) {
+        Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
     }
 
     private fun readFileToByteArray(path: String): ByteArray? {
         val file = File(path)
         if (!file.exists()) {
-            Log.e(TAG, "File doesn't exist!")
+            Log.e(tag, "File doesn't exist!")
             return null
         }
         return try {
             val `in` = FileInputStream(file)
             val inSize: Long = `in`.channel.size() //判断FileInputStream中是否有内容
             if (inSize == 0L) {
-                Log.d(TAG, "The FileInputStream has no content!")
+                Log.d(tag, "The FileInputStream has no content!")
                 return null
             }
             val buffer = ByteArray(`in`.available()) //in.available() 表示要读取的文件中的数据长度
--- app/src/main/java/com/acgdmzy/reader/MainActivity.kt
+++ app/src/main/java/com/acgdmzy/reader/MainActivity.kt
@@ -2,16 +2,25 @@ package com.acgdmzy.reader
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.net.URLEncoder
+
 
 private val PERMISSIONS_STORAGE = arrayOf<String>(
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    Manifest.permission.READ_EXTERNAL_STORAGE,
+    Manifest.permission.WRITE_EXTERNAL_STORAGE
+)
 private const val REQUEST_PERMISSION_CODE = 1
 
 class MainActivity : AppCompatActivity() {
@@ -20,29 +29,89 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.i("Version", Build.VERSION.SDK_INT.toString())
+        // makeStatusBarTransparent()
+        Log.i("Height", getStatusBarHeight().toString())
+
         //申请权限
         requestAllPower()
 
         WebView.setWebContentsDebuggingEnabled(true)
-
         val webView = findViewById<WebView>(R.id.web_view)
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
         webSettings.domStorageEnabled = true
         webSettings.allowFileAccess = true
         webView.addJavascriptInterface(AndroidJs(this), "drive")
-        webView.loadUrl("http://192.168.1.26:8080/#/read")
+
+        val path: String?
+        if (intent.data != null) {
+            val uri = intent.data
+            path = URLEncoder.encode(uri.toString(),"UTF-8")
+        } else {
+            path = getPath("/storage/emulated/0/轻小说/报告！哥哥和我要结婚了！/报告！哥哥和我要结婚了！ 02.epub")
+        }
+        Log.i("Path", path.toString())
+
+        // webView.loadUrl("http://192.168.1.26:8080/#/read/$path")
+        webView.loadUrl("file:///android_asset/dist/index.html#/read/$path")
     }
 
     private fun requestAllPower() {
-        if (ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            ) {
             } else {
-                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE)
+                ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_PERMISSION_CODE
+                )
             }
         }
+    }
+
+    fun Activity.makeStatusBarTransparent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.apply {
+                clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    decorView.systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                } else {
+                    decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                }
+                statusBarColor = Color.TRANSPARENT
+            }
+        }
+    }
+
+    //get StatusBar Height
+    fun getStatusBarHeight(): Int {
+        var height = 0
+        val resourceId =
+            applicationContext.resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            height = applicationContext.resources.getDimensionPixelSize(resourceId)
+        }
+        return height
+    }
+
+    fun getPath(path: String): String {
+        return URLEncoder.encode(
+            path.replace(
+                " ",
+                "%20"
+            ), "UTF-8"
+        )
     }
 }
